fd64049b9b721330a34a6964cbbde8dfa6f12546
Kamaludin Zuhri <kamaludinzuhri@gmail.com>
2026-08-15 21:35:58 +0700

    Fix: Remove duplicate formatBytes() method in SettingController

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -926,17 +926,4 @@ class SettingController extends Controller
         ];
     }
 
-    /**
-     * Format bytes to human readable
-     */
-    private function formatBytes($bytes, $precision = 2)
-    {
-        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
-
-        for ($i = 0; $bytes > 1024 && $i < count($units) - 1; $i++) {
-            $bytes /= 1024;
-        }
-
-        return round($bytes, $precision) . ' ' . $units[$i];
-    }
 }
